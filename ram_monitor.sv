5d7eb85f6375b0f8a79865f9c0c2c5262ff8dca7
Annapurna Shenoy <168350449+Annapurnaa14@users.noreply.github.com>
2026-07-07 01:01:28 +0530

    Updated ram_monitor.sv

--- ram_monitor.sv
+++ ram_monitor.sv
@@ -9,22 +9,13 @@ class ram_monitor;
 
 task run();
     forever begin
-      // Wait for the posedge clk via the clocking block
       @(vif.mon_cb); 
-      
-      // Construct a new packet to capture the sampled values
       captured_trans = new();
-      
-      // Sample values strictly from the clocking block (vif.mon_cb.<signal>)
       captured_trans.address      = vif.mon_cb.address;
       captured_trans.write_enable = vif.mon_cb.write_enb;
       captured_trans.read_enable  = vif.mon_cb.read_enb;
       captured_trans.write_data   = vif.mon_cb.data_in;
-      
-      // This will now capture the steady data output properly aligned with the clock edge
       captured_trans.read_data    = vif.mon_cb.data_out; 
-      
-      // Push the clean transaction to the scoreboard
       mon2sco.put(captured_trans);
     end
   endtask
