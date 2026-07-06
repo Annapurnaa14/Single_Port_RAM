a35748b60a673c7ab0a417068d3efaf9252682e2
Annapurna Shenoy <168350449+Annapurnaa14@users.noreply.github.com>
2026-07-07 00:59:50 +0530

    Update ram_assertion.sv

--- ram_assertion.sv
+++ ram_assertion.sv
@@ -6,13 +6,11 @@ module ram_assertions(
   input [4:0] address
 );
 
-  // Assertion 1: Prevent memory writing during active-low system resets
   property no_write_on_reset;
     @(posedge clk) !reset_n |-> (RAM.memory[address] === 8'hzz);
   endproperty
   assert_no_write_on_reset: assert property(no_write_on_reset);
 
-  // Assertion 2: Ensure data output clears on conflicting multi-requests
   property simultaneous_rw_state;
     @(posedge clk) (write_enb && read_enb) |-> ##1 (RAM.data_out === 8'hz);
   endproperty
